06f39f4e10d3f101ca414b67bbc2f9b20ac999ad
toriajane <victoria.j.emb@gmail.com>
2024-02-10 16:49:20 +0100

    initial

---
+++ script.js
@@ -0,0 +1,16 @@
+function set_animal_pen_space_around() {
+    let animal_pen = document.getElementById("animal-pen");
+    animal_pen.style.justifyContent = "space-around";
+  }
+  function set_animal_pen_space_evenly() {
+    let animal_pen = document.getElementById("animal-pen");
+    animal_pen.style.justifyContent = "space-evenly";
+  }
+  function set_animal_pen_space_between() {
+    let animal_pen = document.getElementById("animal-pen");
+    animal_pen.style.justifyContent = "space-between";
+  }
+  function set_animal_pen_space_none() {
+    let animal_pen = document.getElementById("animal-pen");
+    animal_pen.style.justifyContent = "start";
+  }
