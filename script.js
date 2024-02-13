89069a9ad4705ab8ce43eb1a0ab82c109ec3abe1
toriajane <victoria.j.emb@gmail.com>
2024-02-13 19:53:42 +0100

    syntax

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ function set_animal_pen_space_around() {
     let animal_pen = document.getElementById("animal-pen");
     animal_pen.style.justifyContent = "start";
   }
+
+  /*SYNTAX EXAMPLES*/
+
+  function set_background_color() {
+    let background_color = document.getElementById("dropdown-hamster");
+    let hamster_element = document.getElementById("hamster");
+    hamster_element.style.backgroundColor = background_color.value;
+  }
+
+  function set_border_style() {
+    let border_style = document.getElementById("dropdown-turtle");
+    let turtle_element = document.getElementById("turtle");
+    turtle_element.style.borderStyle = border_style.value;
+  }
